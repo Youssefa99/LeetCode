01c81bf64cc91cf14e39f49c0c11fd0962d169ce
youssef abbas <youssefabbas909@gmail.com.com>
2024-10-06 18:04:57 +0300

    2859. Sum of Values at Indices With K Set Bits

---
+++ BitManipulation/2859.-SumOfValuesAtIndicesWithKSetBits.py
@@ -0,0 +1,40 @@
+"""
+2859. Sum of Values at Indices With K Set Bits
+
+problem desc:
+You are given a 0-indexed integer array nums and an integer k.
+Return an integer that denotes the sum of elements in nums whose corresponding indices have exactly k set bits in their binary representation.
+The set bits in an integer are the 1's present when it is written in binary.
+
+problem difficulty: Easy
+
+Time Complexity: O(NlogN)
+Space Complexity: O(1)
+
+Constraints:
+1 <= nums.length <= 1000
+1 <= nums[i] <= 105
+0 <= k <= 10
+
+solution intuition:
+loop through every element in array checking if number of set bits = k, check for set bits using Brian Kernighan Algorithm
+"""
+
+class Solution:
+    def sumIndicesWithKSetBits(nums: list[int], k: int) -> int:
+        def countSetBits(num: int) -> int:
+            count = 0
+            while num:
+                num = num & (num - 1)
+                count += 1
+            return count
+        n = len(nums)
+        ans = 0
+        for i in range(n):
+            if countSetBits(i) == k:
+                ans += nums[i]
+        return ans
+
+# Example Test Cases
+print(Solution.sumIndicesWithKSetBits([5,10,1,5,2], 1))
+print(Solution.sumIndicesWithKSetBits([4,3,2,1], 2))
