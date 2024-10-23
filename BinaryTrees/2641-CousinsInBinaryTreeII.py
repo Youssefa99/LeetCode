58b58d0f2c7bb57bdcd7956dcc0b9e649c90d4f4
youssef abbas <youssefabbas909@gmail.com.com>
2024-10-23 17:59:37 +0300

    2641. Cousins in Binary Tree II

---
+++ BinaryTrees/2641-CousinsInBinaryTreeII.py
@@ -0,0 +1,66 @@
+"""
+2641. Cousins in Binary Tree II
+
+problem desc:
+Given the root of a binary tree, replace the value of each node in the tree with the sum of all its cousins' values.
+Two nodes of a binary tree are cousins if they have the same depth with different parents.
+Return the root of the modified tree.
+Note that the depth of a node is the number of edges in the path from the root node to it.
+
+Time Complexity: O(N)
+space Complexity: O(N)
+
+Constraints:
+The number of nodes in the tree is in the range [1, 105].
+1 <= Node.val <= 10^4
+
+solution intuition: Two Pass BFS
+use BFS once to calculate the total sum of each level, use BFS one more time this time at each node calculate it's value
+where the new value = node_level_sum - node.val + sibling.val 
+"""
+class Solution:
+    def replaceValueInTree(root):
+        if not root:
+            return root
+        node_queue = deque([root])
+        level_sums = []
+
+        # First BFS: Calculate sum of nodes at each level
+        while node_queue:
+            level_sum = 0
+            level_size = len(node_queue)
+            for _ in range(level_size):
+                current_node = node_queue.popleft()
+                level_sum += current_node.val
+                if current_node.left:
+                    node_queue.append(current_node.left)
+                if current_node.right:
+                    node_queue.append(current_node.right)
+            level_sums.append(level_sum)
+
+        # Second BFS: Update each node's value to sum of its cousins
+        node_queue.append(root)
+        level_index = 1
+        root.val = 0  # Root has no cousins
+        while node_queue:
+            level_size = len(node_queue)
+            for _ in range(level_size):
+                current_node = node_queue.popleft()
+
+                sibling_sum = (
+                    current_node.left.val if current_node.left else 0
+                ) + (current_node.right.val if current_node.right else 0)
+
+                if current_node.left:
+                    current_node.left.val = (
+                        level_sums[level_index] - sibling_sum
+                    )
+                    node_queue.append(current_node.left)
+                if current_node.right:
+                    current_node.right.val = (
+                        level_sums[level_index] - sibling_sum
+                    )
+                    node_queue.append(current_node.right)
+            level_index += 1
+
+        return root
